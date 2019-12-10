afc7ced6be37ca2a221e2b59a617105f4549071f
Will Koste <willkoste@gmail.com>
2019-12-10 10:52:33 -0500

    Final Login Touches

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 const User = require('../models/User');
 
@@ -64,5 +65,12 @@ router.post('/register', (req, res) => {
 
 });
 
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/users/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
 
 module.exports = router;
