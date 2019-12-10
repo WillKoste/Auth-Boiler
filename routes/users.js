b3933026446c109066b5a54d34063ec6c5b86d90
Will Koste <willkoste@gmail.com>
2019-12-10 08:46:53 -0500

    Server Environment Setup

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res) => res.send('Login'));
+router.post('/register', (req, res) => res.send('Register'));
+
+
+module.exports = router;
